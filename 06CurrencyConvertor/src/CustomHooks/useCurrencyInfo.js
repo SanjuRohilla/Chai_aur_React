ca70af5ba64042b42b0108226dd901dfdc230a4e
SanjuRohilla <155614568+SanjuRohilla@users.noreply.github.com>
2024-08-07 20:11:00 +0530

    changes
    Signed-off-by: SanjuRohilla <155614568+SanjuRohilla@users.noreply.github.com>

--- 06CurrencyConvertor/src/CustomHooks/useCurrencyInfo.js
+++ 06CurrencyConvertor/src/CustomHooks/useCurrencyInfo.js
@@ -1,13 +1,22 @@
 import {useEffect , useState} from 'react'
 
+// hooks majorly js return karte, to js file bnana shi rahega
 
 function useCurrencyInfo(currency){
+    const [data,setData] = useState({})            //useState mai hum empty object daal dete hain
     useEffect(() => {
-        fetch()
+        fetch(` https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-0
+            3-06/v1/currencies${currency}.json`)
+            .then((res)  => res.json())
+            .then((res) => setData(res[currency]))
+         console.log(data);
+    } , [currency])
 
-    } , [])
+    console.log(data);
+    return data;
 }
 
+export default useCurrencyInfo;
 
 
 
