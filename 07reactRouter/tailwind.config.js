6ddf54ca18bf374e2abeaed78ab937d705cd060d
SanjuRohilla <155614568+SanjuRohilla@users.noreply.github.com>
2024-08-14 23:17:57 +0530

    rajbhasha
    Signed-off-by: SanjuRohilla <155614568+SanjuRohilla@users.noreply.github.com>

--- 07reactRouter/tailwind.config.js
+++ 07reactRouter/tailwind.config.js
@@ -1,5 +1,3 @@
-import theme from '@material-tailwind/react/theme';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,12 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'hero-pattern': "url('\src\assets\svgs\b58072b9-9ab9-4f4e-afaf-9cdb524b46a3.jpg')",
-        // 'footer-texture': "url('/img/footer-texture.png')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
