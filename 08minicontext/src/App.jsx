e3d44e40c91ca18818e60d1ec1945e91dae2bf4e
SanjuRohilla <155614568+SanjuRohilla@users.noreply.github.com>
2024-09-06 00:25:35 +0530

    context api discussion
    Signed-off-by: SanjuRohilla <155614568+SanjuRohilla@users.noreply.github.com>

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <UserContextProvider>
+      <h1 >Context Api</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 08minicontext/src/context/UserContext.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const  UserContext = React.createContext()
+
+export default UserContext;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
